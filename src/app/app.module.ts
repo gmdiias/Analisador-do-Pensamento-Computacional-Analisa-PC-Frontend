2fefc8900565c45c5fbc37185c1d06c72d6d4d55
Gustavo Mendonça Dias <gmdiias@gmail.com>
2019-11-22 21:52:11 -0300

    melhorias no visual e estilização da tela de resultados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,18 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { MatIconModule } from '@angular/material';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { AppDashboardComponent } from './dashboard/dashboard.component';
-import { AppNavbarComponent } from './navbar/navbar.component';
-import { DashboardModule } from './dashboard/dashboard.module';
-import { MatIconModule } from '@angular/material';
-import { LinguagemModule } from './linguagem/linguagem.module';
-import { CasoTesteModule } from './caso-teste/caso-teste.module';
-import { LinguagemService } from './linguagem/linguagem.service';
-import { CasoTesteService } from './caso-teste/caso-teste.service';
 import { AlunoModule } from './aluno/aluno.module';
 import { AlunoService } from './aluno/aluno.service';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CasoTesteModule } from './caso-teste/caso-teste.module';
+import { CasoTesteService } from './caso-teste/caso-teste.service';
+import { DashboardModule } from './dashboard/dashboard.module';
+import { LinguagemModule } from './linguagem/linguagem.module';
+import { LinguagemService } from './linguagem/linguagem.service';
+import { AppNavbarComponent } from './navbar/navbar.component';
 
 
 @NgModule({
--- src/app/caso-teste/resultado.model.ts
+++ src/app/caso-teste/resultado.model.ts
@@ -3,7 +3,19 @@ export class Resultado {
     hasCondicionalComplexo: boolean;
     hasOperadores: boolean;
     resultadoLogica: number;
+
+    hasLoops: boolean;
+    hasLoopsComplexo: boolean;
+    hasSequencial: boolean;
     resultadoControleFluxo: number;
+
+    hasValoresFixos: boolean;
+    hasVariaveis: boolean;
+    hasListas: boolean;
     resultadoRepresentacao: number;
+
+    hasSequenciais: boolean;
+    hasMultiFuncoes: boolean;
+    hasModulos: boolean;
     resultadoAbstracao: number;
 }
